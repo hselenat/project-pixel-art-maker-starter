334e6ac48c83695d1164fa43aa31f7352918bfce
HSelenat <942430793@qq.com>
2018-03-25 17:50:14 +0800

    complete project-pixel-art-maker-starter

--- README.md
+++ README.md
@@ -1,16 +1,14 @@
-# Pixel Art Maker Project
+# P4像素画项目
 
-## Table of Contents
+## 像素画
 
-* [Instructions](#instructions)
-* [Contributing](#contributing)
+* [提交按钮插入表格](#instructions)
+* [点击像素块填充颜色](#contributing)
 
 ## Instructions
 
-To get started, open `designs.js` and start building out the app's functionality.
-
-For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
+makeGrid（）函数编写调用
 
 ## Contributing
 
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
+添加click监听事件
--- designs.js
+++ designs.js
@@ -1,4 +1,33 @@
-
+    // Select color input
+    let color = "#000";
+    // Select size input
+    
+    // When size is submitted by the user, call makeGrid()
+    function makeGrid() {
+    // Your code goes here!
+        let rowCount = $("#input_height").val();//定义表格行，并获取input高的数值
+        let colCount = $("#input_width").val();//定义表格列，并获取input列的数值
+        //for循环插入表格大小
+        for (let i=0;i<rowCount;i++){
+            $("#pixel_canvas").append("<tr></tr>");//向表格中插入行
+            for (let j=0;j<colCount;j++){
+                $("tr").last("tr").append("<td></td>");//向表格中插入列
+            }
+        }
+    }
+    //当文档加载完毕后，点击提交按钮执行表格插入dom中
+    $(document).ready(function(){
+        $("[type='submit']").click(function(e){
+            e.preventDefault();
+            $('#pixel_canvas').find("tr").remove();//更换数值时移除原先插入的表格
+            makeGrid();
+        });
+    });
+    //点击一个td，对所点中的td进行背景颜色替换
+    $('table').on('click', 'td', function() {
+        color = $('#colorPicker').val(); 
+        $(this).css('background-color', color);
+    });
 
 
 
